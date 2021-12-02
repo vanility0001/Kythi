131a217af527073078557aea75bbbd3a634162b4
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-12-01 20:44:15 -0500

    fix: Bind to 0.0.0.0 for docker

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ server.get("/", async () => {
   return { message: "Hello World!" };
 });
 
-server.listen(process.env.PORT, "127.0.0.1", (err) => {
+server.listen(process.env.PORT, "0.0.0.0", (err) => {
   if (err) throw err;
 
   console.log(`Listening on http://127.0.0.1:${process.env.PORT}/`);
