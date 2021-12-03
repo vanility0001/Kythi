dc9b0b2640a71b4999e85a01d9a48ee1fc473a3f
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-12-03 07:31:50 -0500

    refactor: Include username in verification email

--- src/Utility/Mail.ts
+++ src/Utility/Mail.ts
@@ -16,7 +16,7 @@ export function sendVerifyMail(user: User) {
     from: mailInfo.auth.user,
     to: user.email,
     subject: "Verify your email",
-    html: `Hey!<br><br>Thank you for your intrest in using our serivces. To complete your registration, please verify your email by clicking the link below.<br><br>${process.env.HOST}verify/${user.verificationCode}<br><br>Thanks,<br>The Developer Team at Kythi ❤️<br><br><small>If you did not request this email, feel free to ignore it.</small>`
+    html: `Hey ${user.username}!<br><br>Thank you for your intrest in using our serivces. To complete your registration, please verify your email by clicking the link below.<br><br>${process.env.HOST}verify/${user.verificationCode}<br><br>Thanks,<br>The Developer Team at Kythi ❤️<br><br><small>If you did not request this email, feel free to ignore it.</small>`
   };
 
   return transporter.sendMail(mailOptions);
